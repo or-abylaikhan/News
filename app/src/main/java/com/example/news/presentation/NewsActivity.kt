518c8d8d25684be4e4d7a1ba861be9e813e14565
khan513 <abylaikhan513@gmail.com>
2022-07-09 03:49:10 +0600

    [feature] add bottom app bar

--- app/src/main/java/com/example/news/presentation/NewsActivity.kt
+++ app/src/main/java/com/example/news/presentation/NewsActivity.kt
@@ -1,10 +1,10 @@
 package com.example.news.presentation
 
 import android.os.Bundle
-import com.example.news.databinding.ActivityMainBinding
+import com.example.news.databinding.ActivityNewsBinding
 import com.example.news.util.binding.BindingActivity
 
-class MainActivity : BindingActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+class NewsActivity : BindingActivity<ActivityNewsBinding>(ActivityNewsBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
