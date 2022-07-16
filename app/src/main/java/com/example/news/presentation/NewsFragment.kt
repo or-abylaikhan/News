04a32230d13c73c004fe9d97332396cd5c8f8ba9
khan513 <abylaikhan513@gmail.com>
2022-07-16 18:52:29 +0600

    [feature] add required fragments

---
+++ app/src/main/java/com/example/news/presentation/NewsFragment.kt
@@ -0,0 +1,9 @@
+package com.example.news.presentation
+
+import com.example.news.databinding.FragmentNewsBinding
+import com.example.news.util.binding.BindingFragment
+
+class NewsFragment : BindingFragment<FragmentNewsBinding>(FragmentNewsBinding::inflate) {
+
+
+}
