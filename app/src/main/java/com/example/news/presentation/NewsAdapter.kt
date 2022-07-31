fcf307d0cc165c482a941cfb5a9613f36173b9ff
khan <abylaikhan513@gmail.com>
2022-07-31 23:50:45 +0600

    [feature] add news item layout and adapter

---
+++ app/src/main/java/com/example/news/presentation/NewsAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.news.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.news.databinding.NewsItemBinding
+import com.example.news.domain.model.Article
+import com.example.news.util.publishedAtSimple
+
+class NewsAdapter : ListAdapter<Article, NewsAdapter.NewsViewHolder>(
+    object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(old: Article, new: Article) = old.url == new.url
+        override fun areContentsTheSame(oldItem: Article, newItem: Article) = oldItem == newItem
+    }
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder =
+        NewsViewHolder(NewsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) =
+        holder.bind(getItem(position))
+
+    inner class NewsViewHolder(private val binding: NewsItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(article: Article) {
+            with(binding) {
+                Glide.with(ivArticleImage).load(article.urlToImage).into(ivArticleImage)
+                tvArticleSource.text = article.source.name
+                tvArticleTitle.text = article.title
+                tvArticleDescription.text = article.description
+                tvArticleAuthor.text = article.author
+                tvArticlePublishedAt.text = article.publishedAtSimple()
+            }
+        }
+    }
+}
