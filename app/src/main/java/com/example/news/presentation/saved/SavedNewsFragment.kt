ab2fe1c0c22d4d7b33b8ebfdcedf0a2e818b681d
khan <abylaikhan513@gmail.com>
2022-08-13 18:58:12 +0600

    [feature] implement SavedNewsFragment.kt

--- app/src/main/java/com/example/news/presentation/saved/SavedNewsFragment.kt
+++ app/src/main/java/com/example/news/presentation/saved/SavedNewsFragment.kt
@@ -1,9 +1,43 @@
 package com.example.news.presentation.saved
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.news.R
 import com.example.news.databinding.FragmentSavedNewsBinding
+import com.example.news.presentation.NewsAdapter
+import com.example.news.util.Constants
 import com.example.news.util.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SavedNewsFragment :
-    BindingFragment<FragmentSavedNewsBinding>(FragmentSavedNewsBinding::inflate)
+    BindingFragment<FragmentSavedNewsBinding>(FragmentSavedNewsBinding::inflate) {
+
+    private val viewModel: SaveNewsFragmentViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        iniViews()
+        setupObservers()
+    }
+
+    private fun iniViews() {
+        binding.rvNews.adapter = NewsAdapter(
+            OnItemClickListener = {
+                val bundle = bundleOf(Constants.ARTICLE to it)
+                findNavController().navigate(
+                    R.id.action_savedNewsFragment_to_articleFragment, bundle
+                )
+            }
+        )
+    }
+
+    private fun setupObservers() = viewModel.getSavedArticles().observe(viewLifecycleOwner) {
+        with(binding) {
+            (rvNews.adapter as NewsAdapter).submitList(it)
+        }
+    }
+}
